2b426af80c4d9cad0a21334c023e57dcc7c57238
SiFou01 <seifbaghdad01@gmail.com>
2023-11-11 20:06:25 +0100

    Add new slide function and add slide  buttons

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-let image_index = 1;
+let image_index = 0;
+
+let nextBtn = document.querySelector(".next");
+let previousBtn = document.querySelector(".previous");
+
+nextBtn.addEventListener("click", function() {nextImage()});
+previousBtn.addEventListener("click", function() {previousImage()});
 
 let images = document.querySelectorAll(".image-container img");
 images = Array.from(images);
 
 function nextImage() {
-    if (image_index == 4) {
-        image_index = 1;
+    images.forEach(image => {
+        image.removeAttribute("style", "display : initial");
+    });
+    if (image_index == 3) {
+        image_index = -1;
     }
-    images[image_index - 1].removeAttribute("style", "display : initial");
-    images[image_index].setAttribute("style", "display : initial");
+    images[image_index + 1].setAttribute("style", "display : initial");
     image_index++;
+}
+
+function previousImage() {
+    images.forEach(image => {
+        image.removeAttribute("style", "display : initial");
+    });
+    if (image_index == 0) {
+        image_index = 4;
+    }
+    images[image_index - 1].setAttribute("style", "display : initial");
+    image_index--;
 }
