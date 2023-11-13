422c22f1ae08eb49fc20d8611cdd7e875d80f61e
SiFou01 <seifbaghdad01@gmail.com>
2023-11-13 08:58:48 +0100

    Add text box down animation

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+const down = [
+    {opacity : "1", offset : 0},
+    {opacity : "0", bottom : "-10rem", offset : 0.3},
+    {opacity : "1", offset : 1},
+];
+
+const downDuration = {
+    duration : 1500,
+    iterations : 1,
+    };
+
 let image_index = 0;
 
 let nextBtn = document.querySelector(".next");
 let previousBtn = document.querySelector(".previous");
+
+let imgText = document.querySelector(".image-text");
 
 nextBtn.addEventListener("click", function() {nextImage()});
 previousBtn.addEventListener("click", function() {previousImage()});
@@ -18,6 +31,7 @@ function nextImage() {
     }
     images[image_index + 1].setAttribute("style", "display : initial");
     image_index++;
+    imgText.animate(down, downDuration);
 }
 
 function previousImage() {
