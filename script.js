992917a0064529d509a2cff54353783accc8d64e
SiFou01 <seifbaghdad01@gmail.com>
2023-11-13 10:39:18 +0100

    Add multiple text boxes and animations

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const down = [
     {opacity : "1", offset : 0},
-    {opacity : "0", bottom : "-10rem", offset : 0.3},
-    {opacity : "1", offset : 1},
+    {opacity : "0", bottom : "-10rem", offset : 1},
 ];
 
 const downDuration = {
-    duration : 1500,
+    duration : 700,
     iterations : 1,
     };
 
 let image_index = 0;
+let animate = 0;
 
 let nextBtn = document.querySelector(".next");
 let previousBtn = document.querySelector(".previous");
 
-let imgText = document.querySelector(".image-text");
+let imgText = document.querySelectorAll(".image-text");
+imgText = Array.from(imgText);
 
 nextBtn.addEventListener("click", function() {nextImage()});
 previousBtn.addEventListener("click", function() {previousImage()});
@@ -29,9 +30,14 @@ function nextImage() {
     if (image_index == 3) {
         image_index = -1;
     }
+    imgText[animate].animate(down, downDuration);
+    setTimeout(hide, 700, animate, 1);
     images[image_index + 1].setAttribute("style", "display : initial");
     image_index++;
-    imgText.animate(down, downDuration);
+    animate++;
+    if (animate == 4) {
+        animate = 0;
+    }
 }
 
 function previousImage() {
@@ -41,6 +47,34 @@ function previousImage() {
     if (image_index == 0) {
         image_index = 4;
     }
+    imgText[animate].animate(down, downDuration);
+    setTimeout(hide, 700, animate, 0);
     images[image_index - 1].setAttribute("style", "display : initial");
     image_index--;
+    animate--;
+    if (animate == -1) {
+        animate = 3;
+    }
+}
+
+function hide(animate, slide) {
+    imgText.forEach(text => {
+        text.removeAttribute("style", "display : initial");
+    });
+    if (slide) {
+        if (animate == 3) {
+            imgText[0].setAttribute("style", "display : initial");
+        }
+        else {
+            imgText[animate + 1].setAttribute("style", "display : initial");
+        }
+    }
+    else {
+        if (animate == 0) {
+            imgText[3].setAttribute("style", "display : initial");
+        }
+        else {
+            imgText[animate - 1].setAttribute("style", "display : initial");
+        }
+    }
 }
